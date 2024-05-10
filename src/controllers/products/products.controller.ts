e73c99de8fa57a87d07e3524664e4b2502bad704
Esteban Bedoya Alzate <estebanbedoyaalzate@Estebans-MacBook-Pro.local>
2024-05-10 12:08:34 -0500

    add delete and put methods in products

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -24,5 +33,18 @@ export class ProductsController {
       message: 'acción de crear',
       payload,
     };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') id: string): string {
+    return id;
   }
 }
